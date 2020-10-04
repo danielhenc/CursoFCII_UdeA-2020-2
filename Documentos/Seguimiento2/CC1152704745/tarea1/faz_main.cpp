aaa09a2951c859d808cc1f75c0d93332d5b8fdf4
Jumarulanda <juan.marulandas@udea.edu.co>
2020-10-04 17:37:44 -0500

    Add seg2 tarea1

--- Documentos/Seguimiento2/CC1152704745/tarea1/faz_main.cpp
+++ Documentos/Seguimiento2/CC1152704745/tarea1/faz_main.cpp
@@ -4,30 +4,74 @@
 #include <cstdlib>
 #include <ctime>
 #include <cctype>
+#include <cstring>
 
 using std::srand;
 using std::cout;
 using std::cin;
 using std::endl;
 using std::toupper;
+using std::strcat;
 
-// arreglos de apuntadores a las cadenas de caracteres
-
-const char *arrArt[4] = {"el","un","algún","ningún"};
-const char *arrMayusArt[4] = {"El","Un","Algún","Ningún"};
-const char *arrSust[5] = {"niño","señor","perro","ciudad","carro"};
-const char *arrVrb[5] = {"manejó","saltó","corrió","caminó","pasó"};
-const char *arrPrep[5] = {"a","desde","encima","bajo","sobre"};
-
-int rand_num(int);      // Función para generar números aleatorios
-void print_sentence();  // Función para generar una frase aleatoria
+int rand_num(int);
 
 int main(){
 	srand(time(NULL));
 
-	for (int i=0; i < 20; i++){
-		// ciclo para generar 20 frases aleatorias
-		print_sentence();
+	// arreglos de apuntadores a las cadenas de caracteres
+
+	const char *arrArt[4] = {"el","un","algun","ningun"};
+	const char *arrSust[5] = {"ninio","senior","perro","ciudad","carro"};
+	const char *arrVrb[5] = {"manejo","salto","corrio","camino","paso"};
+	const char *arrPrep[5] = {"a","desde","encima","bajo","sobre"};
+
+	const int len_fras = 30;  // longitud máxima de las frases
+	const int num_fras = 20;  // número de frases a generar
+
+	for (int i=0; i < num_fras; i++){	
+		char fras[len_fras] = "";   // arreglo para guardar las frases
+
+		
+		// se guarda el primer artículo en una cadena de chars para poner el primero en mayúscula
+		int art1 = rand_num(0);
+		char f_art1[5] = {char(toupper(arrArt[art1][0]))};  // se guarda el primer char en mayúscula
+
+		for (int j=1; j <= sizeof(f_art1); j++){
+			// se añaden los demás chars dela artículo
+			f_art1[j] = arrArt[art1][j];
+			if (int(arrArt[art1][j]) == 0){
+				// si se llega al final del artículo, se sale del for
+				break;
+			}
+		}
+	
+		strcat(fras,f_art1);
+		fras[5] = ' '; // se estaba añadiendo un caracter adicional en esta posición
+
+		// se añaden las demás palabras
+
+		int art2 = rand_num(0);    // números aleatorios para las demás palabras
+		int sust1 = rand_num(1);
+		int sust2 = rand_num(1);
+		int vrb = rand_num(1);
+		int prep = rand_num(1);
+
+
+		strcat(fras," ");
+		strcat(fras,arrSust[sust1]);
+		strcat(fras," ");
+		strcat(fras,arrVrb[vrb]);
+		strcat(fras," ");
+		strcat(fras,arrPrep[prep]);
+		strcat(fras," ");
+		strcat(fras,arrArt[art2]);
+		strcat(fras," ");
+		strcat(fras,arrSust[sust2]);
+		strcat(fras,".");
+
+		// finalmente se imprime cada frase
+
+		cout << fras << endl;
 	}
 
 	return 0;
@@ -50,19 +94,3 @@ int rand_num(int n){
 	}
 }
 
-void print_sentence(){
-	// Función para generar frases aleatorias
-
-	// Números aleatorios para escoger las palabras que conforman la frase
-	int art1 = rand_num(0);
-	int art2 = rand_num(0);
-	int sust1 = rand_num(1);
-	int sust2 = rand_num(1);
-	int vrb = rand_num(1);
-	int prep = rand_num(1);
-	
-	// Se imprime cada palabra y se arma la frase. El primer arreglo son los artículos
-	// en mayúsculas, y se termina la frase con un punto.
-	cout << arrMayusArt[art1] << " " << arrSust[sust1] << " " << arrVrb[vrb] << " " << arrPrep[prep];
-	cout  << " "  << arrArt[art2] << " " <<  arrSust[sust2] << "."  << endl;
-}
--- Documentos/Seguimiento2/CC1152704745/tarea1/histaz_main.cpp
+++ Documentos/Seguimiento2/CC1152704745/tarea1/histaz_main.cpp
@@ -4,44 +4,338 @@
 #include <cstdlib>
 #include <ctime>
 #include <cctype>
+#include <cstring>
 
 using std::srand;
 using std::cout;
 using std::cin;
 using std::endl;
 using std::toupper;
+using std::strcat;
 
-// arreglos de apuntadores a las cadenas de caracteres
-
-const char *arrArt[4] = {"el","un","algún","ningún"};
-const char *arrMayusArt[4] = {"El","Un","Algún","Ningún"};
-const char *arrSust[5] = {"niño","señor","perro","ciudad","carro"};
-const char *arrVrb[5] = {"manejó","saltó","corrió","caminó","pasó"};
-const char *arrPrep[5] = {"a","desde","encima","bajo","sobre"};
-
-const char *arrConect[5] = {"entonces ","luego ","sin embargo ","sin saberlo ","mientras "};
-
-const char *arrSust2[5] = {"capucho","agente del ESMAD","papa bomba","zapato","empanada"};
-const char *arrVrb2[5] = {"se comió","se esfumó","zumbó","enseñó","se escapó"};
-const char *arrPrep2[5] = {"contra","hacia","junto","tras","a través"};
-
-const char *arrConect2[5] = {"Finalmente","Sin esperarse","Cuando se creyó todo perdido","Al contrario","Así pues"};
-
-int rand_num(int);      // Función para generar números aleatorios
-
-void print_sentence0();  // Funciónes para generar una frase aleatoria
-void print_sentence1();
-void print_sentence2();
-void print_sentence3();
-
-void print_story();     // Función para generar una historia
+int rand_num(int);
 
 int main(){
 	srand(time(NULL));
-	
-	// Se genera la historia
-	print_story();
 
+	// arreglos de apuntadores a las cadenas de caracteres
+
+	const char *arrArt[4] = {"el","un","algun","ningun"};
+	const char *arrSust[10] = {"ninio","senior","perro","ciudad","carro","capucho","agente del ESMAD","papa bomba","zapato","empanada"};
+	const char *arrVrb[10] = {"manejo","salto","corrio","camino","paso","se comio","se esfumo","zumbo","enseño","se escapo"};
+	const char *arrPrep[10] = {"a","desde","encima","bajo","sobre","contra","hacia","junto","tras","a través"};
+
+	const char *arrConect[5] = {"entonces ","luego ","pero ","sin saberlo ","mientras "};
+
+
+	const char *arrConectF[5] = {"Finalmente ","Sin esperarse ","Cuando se creyo todo perdido ","Valientemente ","Asi pues "};
+
+	const int len_h = 1000;  // longitud máxima de las horias
+
+	// cadena de caracteres para guardar la historia
+	
+	char hist[len_h] = "";
+	int comienzo = 1 + rand() % 2; // número aleatorio entre 1 y 2 para decidir el comienzo de la horia
+
+	if (comienzo == 1){
+		strcat(hist,"Era martes en la tarde y llovia. ");
+	}
+	else{
+		strcat(hist,"En la intersección yacían todos los carros detenidos. ");
+	}
+	
+	// se guarda el primer artículo en una cadena de chars para poner el primero en mayúscula
+	int art1 = rand_num(0);
+	char f_art1[5] = {char(toupper(arrArt[art1][0]))};  // se guarda el primer char en mayúscula
+
+	for (int j=1; j <= sizeof(f_art1); j++){
+		// se añaden los demás chars dela artículo
+		f_art1[j] = arrArt[art1][j];
+		if (int(arrArt[art1][j]) == 0){
+			// si se llega al final del artículo, se sale del for
+			break;
+		}
+	}
+	
+	strcat(hist,f_art1);
+	hist[5] = ' '; // se estaba añadiendo un caracter adicional en esta posición
+
+	// se añaden las demás palabras
+
+	int art2 = rand_num(0);    // números aleatorios para las demás palabras
+	int sust1 = rand_num(2);
+	int sust2 = rand_num(2);
+	int vrb = rand_num(2);
+	int prep = rand_num(2);
+
+
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,", ");
+
+	// se añade una frase adicional con un conector
+
+	int con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConect[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist," ");
+
+	// se añade una frase adicional con un conector
+
+	art1 = rand_num(0);
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,"y ");
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,", ");
+
+	// se añade una frase adicional con un conector
+
+	con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConect[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,". ");
+
+	// Se añade otro ciclo como el anterior.
+
+	// se guarda el primer artículo en una cadena de chars para poner el primero en mayúscula
+	art1 = rand_num(0);
+	char f_art2[5] = {char(toupper(arrArt[art1][0]))};  // se guarda el primer char en mayúscula
+
+	for (int j=1; j <= sizeof(f_art1); j++){
+		// se añaden los demás chars dela artículo
+		f_art2[j] = arrArt[art1][j];
+		if (int(arrArt[art1][j]) == 0){
+			// si se llega al final del artículo, se sale del for
+			break;
+		}
+	}
+	
+	strcat(hist,f_art2);
+	hist[5] = ' '; // se estaba añadiendo un caracter adicional en esta posición
+
+	// se añaden las demás palabras
+
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,", ");
+
+	// se añade una frase adicional con un conector
+
+	con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConect[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist," ");
+
+	// se añade una frase adicional con un conector
+
+	art1 = rand_num(0);
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,"y ");
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,", ");
+
+	// se añade una frase adicional con un conector
+
+	con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConect[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,". ");
+
+	// se añade la parte final de la historia
+	
+	con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConectF[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist," ");
+
+	// se añade una frase adicional con un conector
+
+	art1 = rand_num(0);
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,"y ");
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,", ");
+
+	// se añade una frase adicional con un conector
+
+	con1 = rand_num(1);	
+	art1 = rand_num(0); 
+	art2 = rand_num(0);    // números aleatorios para las demás palabras
+	sust1 = rand_num(2);
+	sust2 = rand_num(2);
+	vrb = rand_num(2);
+	prep = rand_num(2);
+
+	strcat(hist,arrConect[con1]);
+	strcat(hist,arrArt[art1]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust1]);
+	strcat(hist," ");
+	strcat(hist,arrVrb[vrb]);
+	strcat(hist," ");
+	strcat(hist,arrPrep[prep]);
+	strcat(hist," ");
+	strcat(hist,arrArt[art2]);
+	strcat(hist," ");
+	strcat(hist,arrSust[sust2]);
+	strcat(hist,". ");
+
+
+	cout << hist << endl;
+	
 	return 0;
 }
 
@@ -50,126 +344,23 @@ int main(){
 
 int rand_num(int n){
 	// Genera números aleatorios dependiendo si n es 0 o cualquier otr número distinto
-	if (n == 0){
-		// Si n es 0, se generan números aleatorios del 0 al 3 para los 
-		// arreglos que contienen los artículos
-		return rand() % 3;
-	}
-	else{
-		// Si n es distinto de 0, se generan números aleatorios del 0 al 4
-		// para el resto de los arreglos
-		return rand() % 4;
-	}
-}
+	switch(n){
+		case 0:
+			// números aleatorios del 0 al 3 para elegir los artículos
+			return rand() % 3;
+			break;
 
-void print_story(){
-	int inicio = 1 + rand() % 2;
-	int con1 = rand() % 4;
-	int con2 = rand() % 4;
-
-	int conF = rand() % 4;
+		case 1:
+			// números aleatorios del 0 al 4 para elegir los conectores
+			return rand() % 4;
+			break;
 	
-	int conF1 = rand() % 4;
-	int conF2 = rand() % 4;
-
-	// Inicio de la historia, hay dos posibles
-
-	if (inicio == 1){
-		print_sentence0();
-	}
-	else{
-		print_sentence1();
+		case 2:
+			// números aleatorios del 0 al 9 para elegir las demás palabras
+			return rand() % 9;
+			break;
+		
+		default:
+			return 0;
 	}	
-
-	// Cuerpo de la historia
-
-	cout << ", " << arrConect[con1];
-	print_sentence2();
-	cout << ", y como ";
-	print_sentence3();
-	cout << ", ";
-	print_sentence3();
-	cout << ", y " << arrConect[con2];
-	print_sentence2();
-	cout << ". ";
-
-	// Final de la historia
-
-	cout << arrConect2[conF] << " " ;
-	print_sentence3();
-	cout << " puesto que ";
-	print_sentence2();
-	cout << ", " << arrConect[conF1];
-	print_sentence3();
-	cout << ", y " << arrConect[conF2];
-	print_sentence2();
-	cout << "." << endl;
-}
-
-void print_sentence0(){
-	// Función para generar frases aleatorias
-
-	// Números aleatorios para escoger las palabras que conforman la frase
-	int art1 = rand_num(0);
-	int art2 = rand_num(0);
-	int sust1 = rand_num(1);
-	int sust2 = rand_num(1);
-	int vrb = rand_num(1);
-	int prep = rand_num(1);
-	
-	// Se imprime cada palabra y se arma la frase. El primer arreglo son los artículos
-	// en mayúsculas, y se termina la frase con un punto.
-	cout << "Era martes en la tarde y llovía. " << arrMayusArt[art1] << " " << arrSust[sust1] << " " << arrVrb[vrb] << " " << arrPrep[prep];
-	cout  << " "  << arrArt[art2] << " " <<  arrSust[sust2];
-}
-
-void print_sentence1(){
-	// Función para generar frases aleatorias
-
-	// Números aleatorios para escoger las palabras que conforman la frase
-	int art1 = rand_num(0);
-	int art2 = rand_num(0);
-	int sust1 = rand_num(1);
-	int sust2 = rand_num(1);
-	int vrb = rand_num(1);
-	int prep = rand_num(1);
-	
-	// Se imprime cada palabra y se arma la frase. El primer arreglo son los artículos
-	// en mayúsculas, y se termina la frase con un punto.
-	cout << "En la intersección yacían todos los carros detenidos. " << arrMayusArt[art1] << " " << arrSust2[sust1] << " " << arrVrb2[vrb] << " " << arrPrep2[prep];
-	cout  << " "  << arrArt[art2] << " " <<  arrSust2[sust2];
-}
-
-void print_sentence2(){
-	// Función para generar frases aleatorias
-
-	// Números aleatorios para escoger las palabras que conforman la frase
-	int art1 = rand_num(0);
-	int art2 = rand_num(0);
-	int sust1 = rand_num(1);
-	int sust2 = rand_num(1);
-	int vrb = rand_num(1);
-	int prep = rand_num(1);
-	
-	// Se imprime cada palabra y se arma la frase. El primer arreglo son los artículos
-	// en mayúsculas, y se termina la frase con un punto.
-	cout << arrArt[art1] << " " << arrSust2[sust1] << " " << arrVrb[vrb] << " " << arrPrep2[prep];
-	cout  << " "  << arrArt[art2] << " " <<  arrSust[sust2];
-}
-
-void print_sentence3(){
-	// Función para generar frases aleatorias
-
-	// Números aleatorios para escoger las palabras que conforman la frase
-	int art1 = rand_num(0);
-	int art2 = rand_num(0);
-	int sust1 = rand_num(1);
-	int sust2 = rand_num(1);
-	int vrb = rand_num(1);
-	int prep = rand_num(1);
-	
-	// Se imprime cada palabra y se arma la frase. El primer arreglo son los artículos
-	// en mayúsculas, y se termina la frase con un punto.
-	cout << arrArt[art1] << " " << arrSust[sust1] << " " << arrVrb2[vrb] << " " << arrPrep[prep];
-	cout  << " "  << arrArt[art2] << " " <<  arrSust2[sust2];
 }
