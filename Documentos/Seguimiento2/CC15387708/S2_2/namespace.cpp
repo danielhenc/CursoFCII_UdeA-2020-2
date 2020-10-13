af954dff14419bb1f503c98a1d3ad570ae15374d
alexvxela <alex2x62@gmail.com>
2020-10-12 19:39:49 -0500

    Add files via upload

--- Documentos/Seguimiento2/CC15387708/S2_2/namespace.cpp
+++ Documentos/Seguimiento2/CC15387708/S2_2/namespace.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+namespace partenumero
+{
+  int entero( double a )
+  {
+    return a;
+  }
+
+  double partefracc( double b )
+  {
+    return abs(b - entero(b));
+  }
+}
