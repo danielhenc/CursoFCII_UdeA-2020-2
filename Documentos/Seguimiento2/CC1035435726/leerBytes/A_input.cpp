16bde8e96981aa140a38611eddb58f7ce55584de
John Osorio <john.osorioh@udea.edu.co>
2020-10-15 09:03:51 -0500

    archivos seguimiento 3.3

---
+++ Documentos/Seguimiento2/CC1035435726/leerBytes/A_input.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <fstream>
+
+#include <cstdlib>
+
+#include <string>
+
+using namespace std;
+
+void leerBytes(fstream & , long, int); // prototipo
+
+int main() {
+  string nombreArchivoIn = "input.dat";
+  fstream archivoIn(nombreArchivoIn.c_str(), ios:: in );
+  
+  long despl;
+  int n;
+
+  cout << "Ingrese el desplazamiento" << endl;
+  cin >> despl;
+
+  cout << "Ingrese el numero de caracteres a leer" << endl;
+  cin >> n;
+  
+  leerBytes(archivoIn, despl, n);
+
+  return 0;
+}
+
+// obtiene la solicitud del usuario
+void leerBytes(fstream & archivoIn, long despl, int n) {
+  if (archivoIn.fail()) {
+    cout << "No se puede abrir el archivo" << endl;
+    exit(1);
+  }
+  string chars = "";
+  archivoIn.seekg(despl);
+  int i = 0;
+  while (archivoIn.good() && i < n) {
+    chars += archivoIn.get();
+    i += 1;
+  }
+  cout << chars << endl;
+  archivoIn.close();
+
+}
